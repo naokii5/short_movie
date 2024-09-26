b38e9ccb0a032b3e8dcf28dbe484864e917703d1
naokii5 <ik.11.naoki@gmail.com>
2024-09-26 00:29:20 +0000

    chore: Update dependencies and add Jinja2 library for template rendering select topics

--- src/search.py
+++ src/search.py
@@ -7,7 +7,7 @@ import config
 from typing import List
 from pydantic import BaseModel
 from loguru import logger
-
+from jinja2 import Template
 # debug
 import litellm
 litellm.set_verbose=True
@@ -29,6 +29,7 @@ def generate_search_keywords(topic: str) -> list[str]:
     戻り値:
         list[str]: トピックに関連する雑学を検索するためのキーワードのリスト
     """
+    assert isinstance(topic,str)
     keywords = [
         f"{topic} 雑学",
         f"{topic} 豆知識",
@@ -49,17 +50,18 @@ def generate_search_keywords(topic: str) -> list[str]:
 
 def find_interesting_topics(keywords: List[str]) -> str:
     """
-    検索キーワードのリストから、LLMを使用して雑学を検索します。
+    検索キーワードのリストから、LLMを使用して興味深い事実を検索します。
 
     引数:
         keywords (List[str]): 検索キーワードのリスト
 
     戻り値:
-        str: LLMの応答
+        SearchTopicsResponse: LLMの応答
     """
+    assert isinstance(keywords,list)
     prompt = f"""
     # 命令
-    以下のキーワードリストに関する興味深い具体的な事実を教えてください。英語で出力してください。
+    以下のキーワードリストに関する興味深い具体的な事実を教えてください。英語で思考してください。
 
     # 検索キーワード:
     {', '.join(keywords)}
@@ -75,9 +77,46 @@ def find_interesting_topics(keywords: List[str]) -> str:
     logger.info(response.model_dump())
     return response
 
+
+def select_topics(search_topics: SearchTopicsResponse) -> str:
+    """
+    LLMを使用して、検索された事実から特に興味深いものを選択します。
+
+    引数:
+        search_topics (SearchTopicsResponse): 検索された事実のリスト
+
+    戻り値:
+        SearchTopicsResponse: 選択された事実のリスト
+    """
+    assert isinstance(search_topics,SearchTopicsResponse)
+    source = """
+    # 命令
+    以下の候補の中から、確かな情報で、かつ特に興味深いものを3つ選択してください。
+    英語で思考してください。
+
+    候補:
+    {% for search_word in search_topics.search_words %}
+    - {{search_word.keyword}}: {{search_word.fact}}
+    {% endfor %}
+    """
+    template = Template(source=source)
+    prompt = template.render(search_topics=search_topics)
+    logger.info(prompt)
+
+    response = config.client.messages.create(
+    response_model=SearchTopicsResponse,
+    messages=[
+        {"role": "user", "content": prompt},
+    ],
+    )
+    assert isinstance(response,SearchTopicsResponse)
+    return response
+
+    
 if __name__=="__main__":
     topic = "誕生日"
     keywords = generate_search_keywords(topic)
-    response = find_interesting_topics(keywords)
-    print(response)
+    res = find_interesting_topics(keywords)
+    print(res)
 
+    print(select_topics(res))
