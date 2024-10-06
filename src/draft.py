8ef414ab45a30dd25f2ed9c6303055e97282f23c
naokii5 <ik.11.naoki@gmail.com>
2024-10-06 08:06:32 +0000

    feat: Translate content to Japanese if it contains less than 30% Japanese characters

--- src/draft.py
+++ src/draft.py
@@ -1,4 +1,5 @@
 import config
+from translate import translate_with_gemini
 from loguru import logger
 from search import ScrapeData
 from pydantic import BaseModel
@@ -9,7 +10,7 @@ class Draft(BaseModel):
 
 def make_draft(scrape_data_list: list[ScrapeData]) -> Draft:
     """
-    スクレイピングしたデータを元に、下書きを作成します。
+    スクレイピングしたデータを元に下書きを作成します。
 
     引数:
         scrape_data (dict[str, list[tuple[str, str]]): スクレイピングしたデータ
@@ -27,9 +28,11 @@ def make_draft(scrape_data_list: list[ScrapeData]) -> Draft:
     # jinja2を使うとdraftが読み込まれないので、f-stringを使う
     source = f"""
     # 命令
-    以下の内容を参考に、小学生でもなんとなく分かる短い動画の台本を作成してください。
+    以下の内容を参考に、小学生でもなんとなく分かる60秒程度の動画の台本を作成してください。
     動画は淡々とした語り口で進行し、視聴者に知識を提供することを目的としていますが、ときには面白いエピソードを交えても構いません。
     また、情報源の全てを使う必要はありません。必要な情報を選んで使ってください。
+    keywordを書いた後、ナレーションを1行ずつ書いてください。
+
     # 情報源
     """
     prompt = source + draft
@@ -43,7 +46,11 @@ def make_draft(scrape_data_list: list[ScrapeData]) -> Draft:
     )
     logger.info(f"response: {response})")
     assert isinstance(response,Draft)
-    
+    japanese_rate = 1 - sum([chr.isascii() for chr in response.content[0]])/len(response.content[0])
+    threshold = 0.3 # 適当な数字
+    if japanese_rate < threshold:
+        new_content = [translate_with_gemini(content, "English","Japanese") for content in response.content]
+        response.content = new_content
     return response
 
 
