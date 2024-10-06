c13f078d03a784df83ba62f977eeec7e4d33ce68
naokii5 <ik.11.naoki@gmail.com>
2024-10-06 13:04:04 +0000

    feat: Refactor audio function to handle retries for query requests

--- src/voice.py
+++ src/voice.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from loguru import logger
 from time import sleep
-from pathlib import Path
 from draft import Draft
 from pydantic import BaseModel
 import base64
@@ -21,7 +20,18 @@ def audio(draft: Draft, speaker: int)->Voices:
             "text": content,
             "speaker": speaker}
         query_url = VOICEVOX_URL+"audio_query"
-        query = requests.post(query_url, params=query_params)
+        retries = 3
+        for attempt in range(retries):
+            try:
+                query = requests.post(query_url, params=query_params)
+                if not query.status_code==200:
+                    logger.info(query.status_code,query.content)
+                break
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                if attempt == retries - 1:
+                    raise
+                sleep(5)
 
         synthesis_url = VOICEVOX_URL + "synthesis"
         synthesis_params = {
@@ -44,5 +54,12 @@ def audio(draft: Draft, speaker: int)->Voices:
         
 
 if __name__ == "__main__":
-    ...
+    from draft import Draft
+    draft = Draft(keyword="誕生日", content=["誕生日とは、人が生まれた日のことを指します。"])
+    voices = audio(draft, 13)
+    import base64
+    for voice in voices.voice_bytes:
+        with open("test_data/test.wav", "wb") as f:
+            f.write(base64.b64decode(voice))
+
  
