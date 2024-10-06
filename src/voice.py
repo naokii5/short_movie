fac6d9f8cceae9b41d72a999927d0a2f138f6814
naokii5 <ik.11.naoki@gmail.com>
2024-10-06 12:49:08 +0000

    Refactor audio function in voice.py to return voice bytes as a list of strings

--- src/voice.py
+++ src/voice.py
@@ -5,10 +5,16 @@ from loguru import logger
 from time import sleep
 from pathlib import Path
 from draft import Draft
+from pydantic import BaseModel
+import base64
+
 load_dotenv()
 VOICEVOX_URL = os.getenv("VOICEVOX")
 
-def audio(draft: Draft, speaker: int)->list[bytes]:
+class Voices(BaseModel):
+    voice_bytes: list[str]
+
+def audio(draft: Draft, speaker: int)->Voices:
     bytes_list = []
     for content in draft.content:
         query_params = {
@@ -27,30 +33,16 @@ def audio(draft: Draft, speaker: int)->list[bytes]:
                 response = requests.post(synthesis_url, params=synthesis_params, json=request_body,timeout=60)
                 if not response.status_code==200:
                     logger.info(response.status_code,response.content)
-                bytes_list.append(response.content)
+                bytes_list.append(str(base64.b64encode(response.content), 'utf-8'))
                 break
             except Exception as e:
                 logger.error(f"Error: {e}")
                 if attempt == retries - 1:
                     raise
                 sleep(5)
-    return bytes_list
+    return Voices(voice_bytes=bytes_list)
         
 
 if __name__ == "__main__":
-    # from search import URLData, scrape_urls
-    # from draft import make_draft
-    # urls = [URLData(keyword="誕生日", urls=["https://en.wikipedia.org/wiki/Birthday_problem"], meta_data="birthday paradox")]
-    # scrape_data = scrape_urls(urls)
-    # draft = make_draft(scrape_data)
-
-    # with open("sample.txt","w") as f:
-    #     f.write(draft)
-    from draft import Draft
-    draft = Draft(keyword="誕生日", content=["誕生日のパラドックスとは","誕生日が同じ人が何人いるかを求める問題です。"])
-    output_path = Path("test_data/output.wav")
-    bytes_list = audio(draft, 1)
-    for i, bytes_ in enumerate(bytes_list):
-        with open(f"test_data/output_{i}.wav", "wb") as f:
-            f.write(bytes_)
+    ...
  
